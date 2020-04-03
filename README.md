3c3e34fd791022322e6f0848ceff956ace2ad2e4
Pascal Fuhrmann <pascalfuhrmann@protonmail.com>
2020-04-03 17:14:22 +0000

    Addded README, made sure the background image is royalty free and changed the font-color at the starting page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scholar
+
+An online school management tool for students, teachers as well as the school staff written in PHP with the Laravel Framework.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     </style>
     <div class="view" style="background-image: url('{{asset('img/background.jpg')}}'); background-repeat: no-repeat; background-size: cover; background-position: center center; height: 100%; background-blend-mode: darken;">
         <div class="d-flex flex-row justify-content-center align-items-center" style="height: 100%;">
-            <div class="p-4 text-black ">
+            <div class="p-4 text-white">
                 <h1>Welcome {{ Auth::user()->forename }} {{Auth::user()->surname}}.</h1>
             </div>
         </div>
